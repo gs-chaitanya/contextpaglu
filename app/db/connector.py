1daa90b91ffe98b709784a85cd8994ec15625c20
the-dg04 <devgup04@gmail.com>
2025-06-14 16:24:32 +0530

    temp changes

--- app/db/connector.py
+++ app/db/connector.py
@@ -1,4 +1,6 @@
 import pycouchdb
+import uuid
+
 
 class Client:
     def __init__(self):
@@ -53,13 +55,33 @@ class Client:
         return context
     
     def update_context_for_session(self,session_id,new_context):
-        session=self.sessionDB.get(session_id)
         context_doc=self.get_context(session["context_bucket"])
         context_doc["context"]=new_context
         self.contextDB.save(context_doc)
-
+    
+    
+    ## CHAT
+    
+    def add_chat(self,session_id,user_prompt,system_response):
+        new_chat_id=session_id+":"+(uuid.uuid4().hex)
+        self.chatDB.save({
+            "_id":new_chat_id,
+            "user":user_prompt,
+            "response":system_response
+        })
+    
+    def get_all_chats_by_session_id(self,session_id):
+        resp = self.server.get(
+            f"{self.chatDB.name}/_partition/{session_id}/_all_docs",
+            params={"include_docs": "true"}
+        )
+        print(resp)
 
 if __name__=="__main__":
     client=Client()
-    print(client.get_context_for_session("fe51eea2c06c4e3582552726236d7dd4"))
+    # print(client.get_context_for_session("fe51eea2c06c4e3582552726236d7dd4"))
+    # client.add_chat("fe51eea2c06c4e3582552726236d7dd4","e","E")
+    # client.add_chat("fe51eea2c06c4e3582552726236d7dd4","e3","EE")
+    # client.add_chat("fe51eea2c06c4e3582552726236d7dd4","eeee","EEEEE")
+    client.get_all_chats_by_session_id("fe51eea2c06c4e3582552726236d7dd4")
     
